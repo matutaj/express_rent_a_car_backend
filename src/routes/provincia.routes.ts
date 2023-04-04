bae05cbec997b7476748b8307545b978a0e66cc3
matutaj <matutajorge2002@gmail.com>
2023-04-04 12:43:16 +0100

    adicionando router

--- src/routes/provincia.routes.ts
+++ src/routes/provincia.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
 import { CriarProvinciaController } from "../Model/provincia/useCase/criarProvincia/CriarProvinciaController";
+import { ListarProvinciaController } from "../Model/provincia/useCase/listarProvincia/ListarProvinciaController";
+
+
 
 const criarProvincia = new CriarProvinciaController();
+const listarProvincia = new ListarProvinciaController();
 
 const provinciaRoutes = Router();
 provinciaRoutes.post("/", criarProvincia.handle);
+provinciaRoutes.get("/", listarProvincia.handle)
 
 export { provinciaRoutes };
