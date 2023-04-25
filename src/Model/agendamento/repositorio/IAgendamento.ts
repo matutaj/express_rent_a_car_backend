0db4f9c5ec0c051436a5cd3a0d6b674a86c6534b
matutaj <matutajorge2002@gmail.com>
2023-04-25 22:48:44 +0100

    criando interface do Agendamento

---
+++ src/Model/agendamento/repositorio/IAgendamento.ts
@@ -0,0 +1,27 @@
+import { Carro, Empresa, Motorista, Reserva } from "@prisma/client";
+
+export interface DadoAgendamento {
+    id?: string;
+    clienteId: string;
+    carroId: string;
+    dataEntrga: Date
+    dataDevolucao: Date
+    comprovativoUrl: string;
+    nomeAuroporto?: string;
+    nAviao?: string;
+    nAcento?: string;
+    motorista: Motorista
+    nomeHotel?: string;
+    nQuarto?: number
+}
+
+export interface IAgendamento {
+    criar({ }: DadoAgendamento): Promise<Reserva>;
+    listarAgendamentoPorEmpresa(empresaId: string): Promise<Reserva | undefined>;
+    listarEmpresa(empresaId: string): Promise<Empresa | undefined>;
+    listarCarro(carroId: string): Promise<Carro | undefined>;
+    listarAgendamentoPorCliente(clienteId: string): Promise<Reserva | undefined>;
+    listarAgendamentoPorDate(dia: string): Promise<Reserva>
+    atualizarAgendamento({ }: DadoAgendamento): Promise<Reserva>;
+    apagar(id: string): Promise<void>
+}
