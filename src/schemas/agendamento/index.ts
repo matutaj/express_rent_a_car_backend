94d1c26465161743803364b8f5a4f2a84b547217
matutaj <matutajorge2002@gmail.com>
2023-04-27 09:19:02 +0100

    criando o schema

---
+++ src/schemas/agendamento/index.ts
@@ -0,0 +1,11 @@
+import * as Yup from "yup"
+
+const agendamentoschema = Yup.object().shape({
+    clienteId: Yup.string().uuid().required(),
+    carroId: Yup.string().uuid().required(),
+    dataEntrga: Yup.date().required(),
+    dataDevolucao: Yup.date().required(),
+    //  comprovativoUrl:Yup.string().required()
+})
+
+export { agendamentoschema }
