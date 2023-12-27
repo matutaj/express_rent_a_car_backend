aa6bb4d16cc4a8f61ee9af118b854edd6ae6f0a2
matutaj <matutajorge2002@gmail.com>
2023-12-27 20:41:53 +0100

    fix: modifily arquives

--- src/Model/empresa/useCase/listarEmpresaNif/ListarEmpresaNifUseCase.ts
+++ src/Model/empresa/useCase/listarEmpresaNif/ListarEmpresaNifUseCase.ts
@@ -2,21 +2,17 @@ import { Empresa } from "@prisma/client";
 import { EmpresaRepositorio } from "../../repositorio/implementacao/EmpresaRepositorio";
 import { AppError } from "../../../../errors/AppError";
 
-
 class ListarEmpresaNifUseCase {
-    async execute(nif: string): Promise<Empresa> {
-        const repositorio = new EmpresaRepositorio();
+  async execute(nif: string): Promise<Empresa> {
+    const repositorio = new EmpresaRepositorio();
 
-        const existeNifEmpresa = await repositorio.listarNifEmpesa(nif);
+    const existeNifEmpresa = await repositorio.listarNifEmpesa(nif);
 
-        if (!existeNifEmpresa)
-            throw new AppError("Não Existe Empresa Com Este Nif", 400)
+    if (!existeNifEmpresa)
+      throw new AppError("Não Existe Empresa Com Este Nif", 400);
 
-        const result = await repositorio.listarNifEmpesa(nif);
-
-        return result;
-
-    }
+    return existeNifEmpresa;
+  }
 }
 
-export { ListarEmpresaNifUseCase }
+export { ListarEmpresaNifUseCase };
