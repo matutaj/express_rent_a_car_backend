7606581e2fc9cee6c28ad0ca58efb4281cd509e3
matutaj <matutajorge2002@gmail.com>
2023-12-27 19:46:32 +0100

    Feat: add swagger on component

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { clienteRoutes } from "./cliente.routes";
 import { contatoRoutes } from "./contato.routes";
 import { provinciaRoutes } from "./provincia.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "../../src/swagger.json";
 import { tipoContatoRoutes } from "./tipocontato.routes";
 import { municipioRoutes } from "./municipio.routes";
 import { distritoRoutes } from "./distrito.routes";
@@ -11,28 +13,30 @@ import { empresaRoutes } from "./empresa.routes";
 import { carroRoutes, imagemRoutes } from "./carro.routes";
 import { agendaRoutes } from "./agendamento.routes";
 import { reclamacaoRouter } from "./reclamacao.routes";
-import "../config/auth/authWithgoogle"
+import "../config/auth/authWithgoogle";
 import passport from "passport";
 const routes = Router();
 
-
 routes.get("/autenticacaoGoogle", (req, res) => {
-    res.send('auth/google')
-})
+  res.send("auth/google");
+});
 
-routes.get("/auth/google", passport.authenticate('google', { scope: ['email', 'profile'] }))
-
+routes.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 routes.use("/cliente", clienteRoutes);
 routes.use("/tipoContato", tipoContatoRoutes);
 routes.use("/contato", contatoRoutes);
 routes.use("/provincia", provinciaRoutes);
 routes.use("/municipio", municipioRoutes);
 routes.use("/distrito", distritoRoutes);
-routes.use("/login", loginRoutes)
-routes.use("/sessao", sessaoRoutes)
-routes.use("/empresa", empresaRoutes)
-routes.use("/carro", carroRoutes)
-routes.use("/agendamento", agendaRoutes)
+routes.use("/login", loginRoutes);
+routes.use("/sessao", sessaoRoutes);
+routes.use("/empresa", empresaRoutes);
+routes.use("/carro", carroRoutes);
+routes.use("/agendamento", agendaRoutes);
 routes.use("/upload", imagemRoutes);
-routes.use("/reclamacao", reclamacaoRouter)
+routes.use("/reclamacao", reclamacaoRouter);
 export { routes };
